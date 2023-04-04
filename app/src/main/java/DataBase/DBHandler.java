06afbbaa776ed7cf241c31c34e72e800bc06939f
talmi2 <73073201+talmi2@users.noreply.github.com>
2023-04-04 17:15:08 +0300

    commit 13
    Database update, score, player ....

--- app/src/main/java/DataBase/DBHandler.java
+++ app/src/main/java/DataBase/DBHandler.java
@@ -16,14 +16,13 @@ public class DBHandler extends SQLiteOpenHelper {
     public static final String KEY_SCORE = "SCORE"; // Column 3 of the table
 
 
-
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SCORE INTEGER)" );
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SCORE INTEGER)");
 
         db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" + TABLE_NAME + "'");
 
@@ -38,36 +37,36 @@ public class DBHandler extends SQLiteOpenHelper {
 
 
     public boolean insertData(Player player, int i) {
-            SQLiteDatabase db = this.getWritableDatabase();
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(KEY_NAME, player.getName());
-            contentValues.put(KEY_SCORE, player.getScore());
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_NAME, player.getName());
+        contentValues.put(KEY_SCORE, player.getScore());
 
-            int rowsAffected = db.update(TABLE_NAME, contentValues, KEY_ID + " = ?", new String[] { String.valueOf(i) });
-            return (rowsAffected > 0);
+        int rowsAffected = db.update(TABLE_NAME, contentValues, KEY_ID + " = ?", new String[]{String.valueOf(i)});
+        return (rowsAffected > 0);
     }
 
 
     @SuppressLint("Range")
     public Player getPlayerName(int playerId) {
-            SQLiteDatabase db = this.getReadableDatabase();
-            String[] projection = {KEY_ID, KEY_NAME, KEY_SCORE};
-            Cursor cursor = db.query(TABLE_NAME, projection, null, null, null, null, KEY_ID + " ASC", "1");
-            Player player = null;
-            if (cursor.moveToFirst()) {
-                playerId = cursor.getInt(cursor.getColumnIndex(KEY_ID));
-                String playerName = cursor.getString(cursor.getColumnIndex(KEY_NAME));
-                int playerScore = cursor.getInt(cursor.getColumnIndex(KEY_SCORE));
-                player = new Player(playerId, playerName, playerScore);
-            }
-            cursor.close();
-            return player;
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {KEY_ID, KEY_NAME, KEY_SCORE};
+        Cursor cursor = db.query(TABLE_NAME, projection, null, null, null, null, KEY_ID + " ASC", "1");
+        Player player = null;
+        if (cursor.moveToFirst()) {
+            playerId = cursor.getInt(cursor.getColumnIndex(KEY_ID));
+            String playerName = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+            int playerScore = cursor.getInt(cursor.getColumnIndex(KEY_SCORE));
+            player = new Player(playerId, playerName, playerScore);
+        }
+        cursor.close();
+        return player;
     }
 
     @SuppressLint("Range")
     public Player getNextPlayerId(int currentPlayerId) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " +TABLE_NAME + " WHERE " + KEY_ID + "> ? LIMIT 1", new String[]{String.valueOf(currentPlayerId)});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_ID + "> ? LIMIT 1", new String[]{String.valueOf(currentPlayerId)});
         Player nextPlayer = null;
         if (cursor.moveToFirst()) {
             int id = cursor.getInt(cursor.getColumnIndex(KEY_ID));
@@ -79,6 +78,45 @@ public class DBHandler extends SQLiteOpenHelper {
         db.close();
         return nextPlayer;
     }
+
+    @SuppressLint("Range")
+    public int getPlayerScore(int playerId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT " + KEY_SCORE + " FROM " + TABLE_NAME + " WHERE " + KEY_ID + " = ?", new String[]{String.valueOf(playerId)});
+        int score = 0;
+        if (cursor.moveToFirst()) {
+            score = cursor.getInt(cursor.getColumnIndex(KEY_SCORE));
+        }
+        cursor.close();
+        db.close();
+        return score;
+    }
+
+    @SuppressLint("Range")
+    public void updatePlayerScore(int playerId, int newScore) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + KEY_SCORE + " = ? WHERE " + KEY_ID + " = ?";
+        db.execSQL(query, new String[]{String.valueOf(newScore), String.valueOf(playerId)});
+        db.close();
+
+    }
+    @SuppressLint("Range")
+    public Player getPreviousPlayerId(int currentPlayerId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_ID + "< ? ORDER BY " + KEY_ID + " DESC LIMIT 1", new String[]{String.valueOf(currentPlayerId)});
+        Player previousPlayer = null;
+        if (cursor.moveToFirst()) {
+             int id = cursor.getInt(cursor.getColumnIndex(KEY_ID));
+            String name = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+            int score = cursor.getInt(cursor.getColumnIndex(KEY_SCORE));
+            previousPlayer = new Player(id, name, score);
+        }
+        cursor.close();
+        db.close();
+        return previousPlayer;
+    }
+
+
 
     public Cursor getAllData(int i) {
         SQLiteDatabase db = this.getWritableDatabase();
--- app/src/main/java/fr/delcey/mexicangame/Game.java
+++ app/src/main/java/fr/delcey/mexicangame/Game.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -33,6 +36,7 @@ public class Game extends AppCompatActivity {
     private Drawable previousDice1;
     private Drawable previousDice2;
     int currentPlayerId = 1 ;
+    int currentScore = 1;
 
 
     @SuppressLint("MissingInflatedId")
@@ -53,7 +57,12 @@ public class Game extends AppCompatActivity {
         mBackButton = findViewById(R.id.back_button);
         mReveal = findViewById(R.id.print_game);
 
-        final int[] scoring = {32, 41, 42 ,43, 51, 52, 53, 54, 61, 62, 63, 64, 65, 11, 22, 33, 44, 55, 66, 21};
+//        InputFilter[] filters = new InputFilter[1];
+//        filters[0] = new InputFilter.LengthFilter(2);
+//        playerInput.setFilters(filters);
+
+        //int[] scoring = {32, 41, 42 ,43, 51, 52, 53, 54, 61, 62, 63, 64, 65, 11, 22, 33, 44, 55, 66, 21};
+
 
         next_player_button.setEnabled(false);
         roll_again.setEnabled(false);
@@ -67,6 +76,11 @@ public class Game extends AppCompatActivity {
 
         playerName = findViewById(R.id.player_name);
         playerName.setText(player.getName() + " - " + player.getScore());
+
+
+        String strNumber = playerInput.getText().toString().trim();
+
+        currentScore = calculateScore(strNumber);
 
 
 
@@ -88,8 +102,11 @@ public class Game extends AppCompatActivity {
 
                 // save the current image of dice2
                 previousDice2 = dice2.getDrawable();
+
+
                 roll_again.setEnabled(true);
                 rollButton.setEnabled(false);
+                mReveal.setEnabled(false);
 
             }
         });
@@ -106,6 +123,8 @@ public class Game extends AppCompatActivity {
                 dice1.setVisibility(View.INVISIBLE);
                 dice2.setVisibility(View.INVISIBLE);
                 roll_again.setEnabled(false);
+                mReveal.setEnabled(false);
+
 
 
             }
@@ -118,21 +137,33 @@ public class Game extends AppCompatActivity {
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
             }
 
             @Override
             public void afterTextChanged(Editable s) {
                 next_player_button.setEnabled(!s.toString().isEmpty());
                 if (!s.toString().isEmpty()) {
-                    int input = Integer.parseInt(s.toString());
-                    int lastOrder = scoring[0];
-                    if (input < lastOrder) {
-                        playerInput.setError("the score must be greater than or equal to that of the previous player  " + lastOrder);
+                        Intent intent = getIntent();
+                        // receive the value by getStringExtra() method and
+                        // key must be same which is send by first activity
+                        int str = intent.getIntExtra("Score",currentScore);
+
+                        int score_previous_player = str;
+                        int input = Integer.parseInt(s.toString());
+                        int score = calculateScore(String.valueOf(input));
+                        if (score < score_previous_player) {
+                            playerInput.setError("the score must be greater than or equal to that of the previous player " + getScore(score_previous_player));
+                        }
+//                        else if(score_previous_player == 21){
+//                            joker
+//                        }
+//                        else if(score_previous_player == 20){
+//                            mexicain
+//                        }
                     }
+
                 }
-            }
         });
         next_player_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,31 +178,43 @@ public class Game extends AppCompatActivity {
                 }
                 else{
 
-                    dice1.setImageDrawable(previousDice1);
+                    int[] actual = getDiceScore(previousDice1, previousDice2);
 
-                    // set the image of dice2 to the previous image
-                    dice2.setImageDrawable(previousDice2);;
+                    if(actual[0] > actual[1]){
+                        dice1.setImageDrawable(previousDice1);
+
+                        // set the image of dice2 to the previous image
+                        dice2.setImageDrawable(previousDice2);
+                    }
+                    else{
+                        dice1.setImageDrawable(previousDice2);
+
+                        // set the image of dice2 to the previous image
+                        dice2.setImageDrawable(previousDice1);
+                    }
 
                     dice1.setVisibility(View.INVISIBLE);
                     dice2.setVisibility(View.INVISIBLE);
 
-                        rollButton.setEnabled(true);
-                        roll_again.setEnabled(false);
+                    rollButton.setEnabled(true);
+                    roll_again.setEnabled(false);
+                    mReveal.setEnabled(true);
 
-                        nextPlayer();
-                        // Add the player ID as an extra to the Intent
-                        Intent intent = new Intent(Game.this, Game.class);
+
+                    nextPlayer();
+                    // Add the player ID as an extra to the Intent
+                    Intent intent = new Intent(Game.this, Game.class);
+                    currentScore = calculateScore(strNumber);
+                    intent.putExtra("Score", currentScore);
 
 //                    } else { // If there is no player with the next ID in the database, show an error message
 //                        Toast.makeText(Game.this, "No more players", Toast.LENGTH_SHORT).show();
 ////                        currentPlayerId--; // decrement current player ID as there are no more players
 //                    }
 
-//                    intent.putExtra("numPlayers", strNumber);
-//                    startActivityForResult(intent,5);
+//
                 }
 
-//                Intent intent = new Intent(Game.this,Game.class);
 
             }
         });
@@ -182,14 +225,123 @@ public class Game extends AppCompatActivity {
                 dice1.setVisibility(View.VISIBLE);
                 dice2.setVisibility(View.VISIBLE);
 
-                dice1.setImageDrawable(previousDice1);
 
-                // set the image of dice2 to the previous image
-                dice2.setImageDrawable(previousDice2);
+                int[] actual = getDiceScore(previousDice1, previousDice2);
+
+                if(actual[0] > actual[1]){
+                    dice1.setImageDrawable(previousDice1);
+
+                    // set the image of dice2 to the previous image
+                    dice2.setImageDrawable(previousDice2);
+                }
+                else{
+                    dice1.setImageDrawable(previousDice2);
+
+                    // set the image of dice2 to the previous image
+                    dice2.setImageDrawable(previousDice1);
+                }
+
+                String p = String.valueOf(actual[0]) + String.valueOf(actual[1]);
+                int c = calculateScore(p);
+//                Intent intent = getIntent();
+//                // receive the value by getStringExtra() method and
+//                // key must be same which is send by first activity
+//                int str = intent.getIntExtra("Score",currentScore);
+
+                String s = playerInput.getText().toString().trim();
+                //int score_previous_player = str;
+                int input = Integer.parseInt(s);
+                int score = calculateScore(String.valueOf(input));
+
+                if(c <= score){
+                    subtractPointFromPlayer(currentPlayerId);
+
+                    System.out.println(" c moi " + currentPlayerId);
+                    String toast = player.getName() + " actu lost a point";
+                    System.out.println(player.getName());
+                    Toast.makeText(Game.this, toast, Toast.LENGTH_SHORT).show();
+                }
+                else if(c > score){
+                    //Player prev_player = myDb.getPreviousPlayerId(currentPlayerId);
+
+
+                    String toast = player.getName() + " prev lost a point";
+                    Toast.makeText(Game.this, toast, Toast.LENGTH_SHORT).show();
+                    currentPlayerId = (currentPlayerId - 1);
+                    subtractPointFromPlayer(currentPlayerId);
+
+
+                    playerName.setText(player.getName() + " - " + player.getScore());
+
+                    Intent intent = new Intent(Game.this, Game.class);
+                }
+
+
+
+                System.out.println("c mon score ancien " + c + " mon nouveau "+ score);
 
             }
         });
     }
+    public void subtractPointFromPlayer(int playerId) {
+        // Get the current score of the player
+        int currentScore = myDb.getPlayerScore(playerId);
+
+
+        // Subtract 1 from the score
+        int newScore = currentScore - 1;
+        // Update the player's score in the database
+        myDb.updatePlayerScore(playerId, newScore);
+        if(newScore == 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+            builder.setTitle("You lost")
+                    .setMessage("Would you like a last chance?")
+                    .setPositiveButton("Yes, pleas", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(Game.this, TruthOrDare.class);
+                            startActivityForResult(intent,5);
+                        }
+                    })
+                    .setNegativeButton("No, thank you", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(Game.this, Game.class);
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+
+    }
+
+    private int[] getDiceScore(Drawable dice1, Drawable dice2) {
+        Drawable[] diceImages = {null,getResources().getDrawable(R.drawable.d_1), getResources().getDrawable(R.drawable.d_2),
+                getResources().getDrawable(R.drawable.d_3), getResources().getDrawable(R.drawable.d_4),
+                getResources().getDrawable(R.drawable.d_5), getResources().getDrawable(R.drawable.d_6)};
+
+        int score1 = 0;
+        if (dice1 != null) {
+            for (int i = 1; i < diceImages.length; i++) {
+                if (dice1.getConstantState().equals(diceImages[i].getConstantState())) {
+                    score1 = i;
+                    break;
+                }
+            }
+        }
+
+        int score2 = 0;
+        if (dice2 != null) {
+            for (int i = 1; i < diceImages.length; i++) {
+                if (dice2.getConstantState().equals(diceImages[i].getConstantState())) {
+                    score2 = i;
+                    break;
+                }
+            }
+        }
+
+        return new int[] {score1, score2};
+    }
+
     private void rollDice() {
         Random random = new Random();
         int diceValue1 = random.nextInt(6) + 1;
@@ -237,6 +389,107 @@ public class Game extends AppCompatActivity {
                 break;
         }
     }
+
+    public static int calculateScore(String input) {
+        switch (input) {
+            case "32":
+                return 1;
+            case "41":
+                return 2;
+            case "42":
+                return 3;
+            case "43":
+                return 4;
+            case "51":
+                return 5;
+            case "52":
+                return 6;
+            case "53":
+                return 7;
+            case "54":
+                return 8;
+            case "61":
+                return 9;
+            case "62":
+                return 10;
+            case "63":
+                return 11;
+            case "64":
+                return 12;
+            case "65":
+                return 13;
+            case "11":
+                return 14;
+            case "22":
+                return 15;
+            case "33":
+                return 16;
+            case "44":
+                return 17;
+            case "55":
+                return 18;
+            case "66":
+                return 19;
+            case "21":
+                return 20;
+            case "31":
+                return 21;
+            default:
+                return 1;
+        }
+    }
+
+    public static int getScore(int input) {
+        switch (input) {
+            case 1:
+                return 32;
+            case 2:
+                return 41;
+            case 3:
+                return 42;
+            case 4:
+                return 43;
+            case 5:
+                return 51;
+            case 6:
+                return 52;
+            case 7:
+                return 53;
+            case 8:
+                return 54;
+            case 9:
+                return 61;
+            case 10:
+                return 62;
+            case 11:
+                return 63;
+            case 12:
+                return 64;
+            case 13:
+                return 65;
+            case 14:
+                return 11;
+            case 15:
+                return 22;
+            case 16:
+                return 33;
+            case 17:
+                return 44;
+            case 18:
+                return 55;
+            case 19:
+                return 66;
+            case 20:
+                return 21;
+            case 21:
+                return 31;
+            default:
+                return 32;
+        }
+    }
+
+
+
 
     public void viewAll(){
         btnviewAll.setOnClickListener(
